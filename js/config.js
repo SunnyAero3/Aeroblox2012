5d7c33951e47c22209e32c54e4fac03efed4b098
SunnyAero3 <jake.hili3231@gmail.com>
2026-08-02 21:16:26 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // js/config.js
 
-const SUPABASE_URL = 'https://hvxezfwdgskwldclvlprm.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_bVf9JahB15fNjLlIGIlzeA_FGsRJ6jT';
+const SUPABASE_URL = 'https://hvxezfwdgskwldcfvtpm.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2eGV6ZndkZ3Nrd2xkY2Z2dHBtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU2NjgzOTMsImV4cCI6MjEwMTI0NDM5M30.YOq9nvgmEszFvfVfYUetSdfcrJGofFuozHSmH57rmXY';
 
 // Initialize Supabase client with a unique name
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
