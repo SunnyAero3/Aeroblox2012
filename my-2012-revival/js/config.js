137f404a776d33e52f4696e5ad7d9d406ef97cd1
SunnyAero3 <jake.hili3231@gmail.com>
2026-08-02 15:14:26 +0200

    Update config.js

--- my-2012-revival/js/config.js
+++ my-2012-revival/js/config.js
@@ -1,1 +1,7 @@
-// placeholder: empty file
+// js/config.js
+
+const SUPABASE_URL = 'https://hvxezfwdgskwldclvlprm.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_bVf9JahB15fNjLlIGIlzeA_FGsRJ6jT';
+
+// Initialize Supabase
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
