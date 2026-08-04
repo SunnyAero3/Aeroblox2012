7047ef19dd4637c464b128fc121de66fab9596e7
SunnyAero3 <jake.hili3231@gmail.com>
2026-08-04 12:04:50 +0200

    Increase background music volume to 30%

--- js/music.js
+++ js/music.js
@@ -2,7 +2,7 @@
 const audioPath = 'audio/DJ Glejs - Better Off Alone (Remix).mp3'; 
 const bgMusic = new Audio(encodeURI(audioPath));
 bgMusic.loop = true;
-bgMusic.volume = 0.25; // Set volume to 25%
+bgMusic.volume = 0.3; // Set volume to 30%
 
 document.addEventListener('click', (event) => {
   const isInteractive = event.target.closest('a, button, input, select, textarea');
